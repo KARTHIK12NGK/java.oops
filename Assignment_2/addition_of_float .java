8aa99b7466fac2ea750075d304ada52b8dd33bb7
KARTHIK12NGK <90174519+KARTHIK12NGK@users.noreply.github.com>
2022-01-04 22:15:05 +0530

    Create addition_of_float .java

---
+++ Assignment_2/addition_of_float .java
@@ -0,0 +1,16 @@
+//Program To Add Two floating point Numbers
+class addfloat
+{
+  float c=10.f;
+  float d=10.f;
+}
+public class addition_of_float extends addfloat
+{
+    float add=c+d;
+    public static void main(String args[])
+    {
+        addition_of_float obj= new addition_of_float();
+        System.out.print("addittion of float:"+obj.add);
+    }
+    
+}
